b7e2cabe238734b0c7eecb8a99aa7df30ed4db28
VZON7 <127258768+VZON7@users.noreply.github.com>
2024-07-22 04:37:29 +1000

    Week8_01
    Lobby_create 4 stand with camera, add C++ (MGGamemode)

---
+++ Source/MGD_Template/Public/MGGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MGGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MGD_TEMPLATE_API AMGGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
