99e5bffa00751b3770ddacbddf7f9a12c2df3c8f
VZON7 <127258768+VZON7@users.noreply.github.com>
2024-07-26 19:33:01 +1000

    Week9_01
    paste OSS EOS into vs code ,change something in C# MGD_Template.Build.cs

--- Source/MGD_Template/MGD_Template.Build.cs
+++ Source/MGD_Template/MGD_Template.Build.cs
@@ -8,7 +8,11 @@ public class MGD_Template : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
+			"OnlineSubsystemUtils","OnlineSubsystemEOS","OnlineSubsystemEOSPlus"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
@@ -16,7 +20,7 @@ public class MGD_Template : ModuleRules
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
