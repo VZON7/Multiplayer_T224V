dbd816fe510cca338a967dc3c3ebc7156fc94ddd
VZON7 <127258768+VZON7@users.noreply.github.com>
2024-07-26 23:14:07 +1000

    Week9_03
    we have access to Epic Online Services in our game

--- Source/MGD_Template/Private/MGGameInstance.cpp
+++ Source/MGD_Template/Private/MGGameInstance.cpp
@@ -2,4 +2,37 @@
 
 
 #include "MGGameInstance.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSubsystemUtils.h"
 
+void UMGGameInstance::LoginEOS()
+{
+	// this is the online subsystem
+	// this gives us access to all of the online subsystem function
+	const IOnlineSubsystem* ossRef = Online::GetSubsystem(GetWorld());
+
+	// if the ossRef returns null
+	// exit the function
+	if (!ossRef)
+		return;
+
+	// this is the identity interface for EOS
+	// we can get info about the players account from this interface
+	// it also allows us to access authentication(Login / Logout)
+	const IOnlineIdentityPtr identityRef = ossRef->GetIdentityInterface();
+
+	// if the identityRef returns null
+	// exit the function
+	if (!identityRef)
+		return;
+
+	// this sets up the type of account credentials that the login will expect
+	// the account portal is a login portal that opens when you try to login
+	FOnlineAccountCredentials accCreds;
+	accCreds.Id = FString();
+	accCreds.Token = FString();
+	accCreds.Type = FString(TEXT("accountportal"));
+
+	// run the login portal (technically the main function we really need to run)
+	identityRef->Login(0, accCreds);
+}
--- Source/MGD_Template/Public/MGGameInstance.h
+++ Source/MGD_Template/Public/MGGameInstance.h
@@ -14,4 +14,15 @@ class MGD_TEMPLATE_API UMGGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
+	// allows other classes to use that function
+public:
+	UFUNCTION(BlueprintCallable, Category="Login")
+	void LoginEOS();
+
+	// only this class and derived/child classes can access this function
+protected:
+	
+	// only this class can access these private functions
+private:
+	
 };
