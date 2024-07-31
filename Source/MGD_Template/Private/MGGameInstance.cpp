9e4b0cfe4336ef9267322b90ac934c8d366e3753
VZON7 <127258768+VZON7@users.noreply.github.com>
2024-07-31 21:31:31 +1000

    Week10_01
    look like is successful to login and host game but the start button doesn't show up

--- Source/MGD_Template/Private/MGGameInstance.cpp
+++ Source/MGD_Template/Private/MGGameInstance.cpp
@@ -4,6 +4,9 @@
 #include "MGGameInstance.h"
 #include "OnlineSubsystem.h"
 #include "OnlineSubsystemUtils.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "OnlineSessionSettings.h"
+#include "Online/OnlineSessionNames.h"
 
 void UMGGameInstance::Init()
 {
@@ -30,8 +33,17 @@ void UMGGameInstance::Init()
 
 	// binding the login complete function to login complete in the identity interface
 	identityRef->OnLoginCompleteDelegates->AddUObject(this, &UMGGameInstance::EOSLoginComplete);
-}
 
+	const IOnlineSessionPtr sessionRef = ossRef->GetSessionInterface();
+
+	if (!sessionRef)
+		return;
+
+	// binding our SessionCreateComplete function to the CreateSession complete delegate
+	// when a session is created (success or fail) this function will run
+	sessionRef->OnCreateSessionCompleteDelegates.AddUObject(this, &UMGGameInstance::SessionCreateComplete);
+}
+	
 void UMGGameInstance::LoginEOS()
 {
 	// this is the online subsystem
@@ -99,8 +111,62 @@ FString UMGGameInstance::GetDisplayName() const
 	return identityRef->GetPlayerNickname(0);
 }
 
+bool UMGGameInstance::IsInSession() const
+{
+	// are we logged in
+	// this checks if there is an online subsystem so we can skip that check
+	if (!IsLoggedIn())
+		return false;
+
+	// get the interface that interacts with sessions so we can get session data
+	const IOnlineSessionPtr sessionRef = Online::GetSubsystem(GetWorld())->GetSessionInterface();
+
+	// make sure there is a session interface
+	if (sessionRef)
+		return false;
+
+	// get locally store the session state
+	const EOnlineSessionState::Type state = sessionRef->GetSessionState(MGSESSION_NAME);
+	
+	// if there is a session return true
+	return state != EOnlineSessionState::NoSession;
+}
+
+void UMGGameInstance::HostGame(bool lan)
+{
+	// are we logged in
+	if (!IsLoggedIn())
+		return;
+
+	// get the session interface
+	const IOnlineSessionPtr sessionRef = Online::GetSubsystem(GetWorld())->GetSessionInterface();
+
+	if (!sessionRef)
+		return;
+
+	// define the setting of the session
+	FOnlineSessionSettings settings;
+	settings.NumPublicConnections = 4;
+	settings.bIsLANMatch = lan;
+	settings.bIsDedicated = false;
+	settings.bAllowInvites = true;
+	settings.bShouldAdvertise = true;
+	settings.bUsesPresence = true;
+	settings.bUseLobbiesIfAvailable = true;
+	settings.Set(SEARCH_KEYWORDS, MGSESSION_NAME.ToString(), EOnlineDataAdvertisementType::ViaOnlineService);
+
+	//create the session using the settings and name we created 
+	sessionRef->CreateSession(0, MGSESSION_NAME, settings);
+}
+
 void UMGGameInstance::EOSLoginComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId,
                                        const FString& Error)
 {
 	OnLoginComplete(bWasSuccessful, Error);
+}
+
+void UMGGameInstance::SessionCreateComplete(FName SessionName, bool bWasSuccessful)
+{
+	EnableListenServer(true);
+	OnSessionCreateComplete(bWasSuccessful);
 }
--- Source/MGD_Template/Public/MGGameInstance.h
+++ Source/MGD_Template/Public/MGGameInstance.h
@@ -6,6 +6,8 @@
 #include "Engine/GameInstance.h"
 #include "MGGameInstance.generated.h"
 
+#define MGSESSION_NAME FName(TEXT("MGSESSION"))
+
 /**
  * 
  */
@@ -26,14 +28,27 @@ public:
 
 	UFUNCTION(BlueprintPure, Category="User")
 	FString GetDisplayName() const;
+
+	UFUNCTION(BlueprintPure, Category="Session")
+	bool IsInSession() const;
+
+	// host a game using EOS - currently online LAN capable - THANKS EPIC!!!
+	UFUNCTION(BlueprintCallable,Category="Session")
+	void HostGame(bool lan);
 	
 	// only this class and derived/child classes can access this function
 protected:
 	UFUNCTION(BlueprintImplementableEvent, Category="Login")
 	void OnLoginComplete(bool success, const FString& error);
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Session") 
+	void OnSessionCreateComplete(bool success);
 	
 	// only this class can access these private functions
 private:
 	// runs when eos login complete
 	void EOSLoginComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error);
+
+	// run when the session create function completes
+	void SessionCreateComplete(FName SessionName, bool bWasSuccessful);
 };
